ea9d7b17be1a9153746b6217c02016b3e6add31b
井上達朗 <t.inoue@inouetatsurounoMacBook-puro.local>
2019-01-17 17:06:59 +0900

    Add Family number

--- app/controllers/phones_controller.rb
+++ app/controllers/phones_controller.rb
@@ -6,6 +6,7 @@ class PhonesController < ApplicationController
   	search_cost = params[:cost]
   	search_value = params[:value]
   	@plans = Plan.where(["cost <= ? and value >= ?", search_cost, search_value])
+  	@number_of_family = params[:number_of_family]
   	unless @plans.present?
   		render html:"結果がありません"
   	end

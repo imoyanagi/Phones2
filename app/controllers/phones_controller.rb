35239090537340a78f7477ff4841a1efd593e3a1
井上達朗 <t.inoue@inouetatsurounoMacBook-puro.local>
2019-01-17 15:59:15 +0900

    Add no result message

--- app/controllers/phones_controller.rb
+++ app/controllers/phones_controller.rb
@@ -6,6 +6,9 @@ class PhonesController < ApplicationController
   	search_cost = params[:cost]
   	search_value = params[:value]
   	@plans = Plan.where(["cost <= ? and value >= ?", search_cost, search_value])
+  	unless @plans.present?
+  		render html:"結果がありません"
+  	end
   end
 
   def show

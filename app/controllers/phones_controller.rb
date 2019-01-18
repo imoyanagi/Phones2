2979932fdc0edc1d3659b3efe715007e69b1d101
井上達朗 <t.inoue@inouetatsurounoMacBook-puro.local>
2019-01-18 16:28:26 +0900

    TUKAENAI

--- app/controllers/phones_controller.rb
+++ app/controllers/phones_controller.rb
@@ -7,24 +7,48 @@ class PhonesController < ApplicationController
   def search
   	search_cost = params[:cost]
   	search_value = params[:value]
-  	@plans = Plan.where(["cost <= ? and value >= ?", search_cost, search_value])
-  	@number_of_family = params[:number_of_family]
-  	unless @plans.present?
-  		render html:"結果がありません"
-  	end
+	search_call_time = params[:call_time]
+  	@plans = Plan.where(" value >= ? ", search_value )
+  	@call_plans = CallPlan.where("call_time == ? ", search_call_time )
+ # ---------------使えない-----------------
+  	# @total_costs = @plans.product(@call_plans)
+    # @plans.product(@call_plans).each do |data, call, total_cost|
+	# 	if data.career_id == call.career_id
+	# 		total_cost = data.cost + call.cost
+	# 	end
+	# end
+ # ------使える------------------------
+  	# @number_of_family = params[:number_of_family]
+  	# unless @plans.present?
+  	# 	render html:"結果がありません"
+  	# end
   end
 
-  def calc_total_cost
-  	search_cost = params[:cost]
-  	search_value = params[:value]
-  	search_call_time = params[:call_time]
-  	@plans = Plan.where("value >= search_value")
-  	@call_plans = CallPlan.where("call_time == search_call_time")
-  	# @plans.each do |data_cost,call_cost|
-  	# @total_costs = @call_plans.cost + @plans.cost
-  	# if @total_costs =
-  end
+  # def calc_total_cost
+  # 	search_cost = params[:cost]
+  # 	search_value = params[:value]
+  # 	search_call_time = params[:call_time]
+  # 	@plans = Plan.where("value >= search_value")
+  # 	@call_plans = CallPlan.where("call_time == search_call_time")
+  # end
 
   def show
   end
 end
+
+
+# 1GBのプラン取ってくる
+# @plans = [ {name:"ベーシック〜１GB", career_id: 1, }, {name: "ベーシック〜３GB", career_id: 1}, {name: "ぴたっと〜１GB", career_id: 2} ]
+
+# ５分通話プラン取ってくる
+# @call_plans = [ { name:"カケホーダイライトプラン", career_id: 1}, {name: "スーパーカケホ", career_id: 2} ]
+
+# データと通話で同じキャリアのものを足し算
+
+# 数は@plansの数だけ存在する
+
+
+
+
+
+

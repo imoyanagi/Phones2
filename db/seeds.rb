7980f0aa5cd6a3afb921669d5952542a48f63647
井上達朗 <t.inoue@inouetatsurounoMacBook-puro.local>
2019-01-23 14:18:50 +0900

    change seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -9,10 +9,12 @@ s =1
 while s <= 22
 	c = 1
 	while c <= 15
-		m = 1
-		while m <= 9
-		Total.create(plan_id: s, call_plan_id: c, mobile_phone_id: m)
-		m += 1
+		if s <= 4 && c <= 3 or s.between?(5,11) && c.between?(4,6) or s.between?(12,16) && c.between?(7,9) or s.between?(17,19) && c.between?(10,12) or s.between?(20,22) && c.between?(13,15)
+			m = 1
+			while m <= 9
+			Total.create(plan_id: s, call_plan_id: c, mobile_phone_id: m)
+			m += 1
+			end
 		end
 	c += 1
 	end
